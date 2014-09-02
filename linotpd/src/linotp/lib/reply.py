c0152608fa2ee1b1f207663ead88e5347cde662c
Richard May <richarddlmay@users.noreply.github.com>
2014-09-02 11:35:57 +0100

    Fix JSON return structure.

--- linotpd/src/linotp/lib/reply.py
+++ linotpd/src/linotp/lib/reply.py
@@ -207,7 +207,7 @@ def sendResult(response, obj, id=1, opt=None):
            "id": id }
 
     if opt is not None and len(opt) > 0:
-        res["detail"] = opt
+        res["value"]["detail"] = opt
 
     return json.dumps(res, indent=3)
 
